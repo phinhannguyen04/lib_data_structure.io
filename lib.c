c4ed603bd44943dbe93553bd810b94f98e048d1f
phinhannguyen04 <144946023+phinhannguyen04@users.noreply.github.com>
2023-10-25 18:55:57 +0700

    Add files via upload
    danh sách đặc

---
+++ lib.c
@@ -0,0 +1,205 @@
+#include <stdio.h>
+#include <conio.h>
+
+#define Maxlength 30
+
+typedef int ElementType;
+
+typedef int Position;
+
+typedef struct
+{
+
+    ElementType Elements[Maxlength];
+
+    Position Last;
+
+} List;
+
+// tao danh sach rong
+
+void MakeNull_List(List *L)
+{
+    L->Last = 0;
+}
+
+// kiem tra danh sach rong
+int Empty_List(List L)
+{
+    return (L.Last == 0);
+}
+
+// kiem tra danh sach co day hay khong, neu day tra ve 1 nguoc lai tra ve 0
+int Full_List(List L)
+{
+    return (L.Last == Maxlength);
+}
+
+// tra ve phan tu dau tien trong danh sach
+
+Position FirstList(List L)
+{
+    return 1;
+}
+
+// Tra ve phan tu cuoi trong danh sach
+Position EndList(List L)
+{
+    return L.Last + 1;
+}
+
+// tra ve vi tri sau p next (vi tri p + 1)
+Position Next(Position P, List L)
+{
+    return P + 1;
+}
+
+// tra ve vi tri truoc p position (vi tri p - 1)
+Position Previous(Position P, List L)
+{
+    return P - 1;
+}
+
+// tra ve noi dung phan tu tai vi tri P
+ElementType Retrieve(Position P, List L)
+{
+    return L.Elements[P - 1];
+}
+
+// Them phan tu co noi dung X vao vi tri P
+void Insert_List(ElementType X, Position P, List *L)
+{
+    int i = 0;
+
+    if ((L->Last) == Maxlength)
+    {
+        printf("Danh sach day");
+    }
+    else
+    {
+        if ((P < 1) || (P > L->Last + 1))
+        {
+
+            printf("vi tri khong hop le");
+        }
+        else
+        {
+            for (i = L->Last; i >= P; i--)
+            {
+
+                L->Elements[i] == L->Elements[i - 1];
+            }
+
+            L->Last++;
+
+            L->Elements[P - 1] = X;
+        }
+    }
+}
+
+// Xoa phan tu tai vi tri P trong danh sach
+void Delete_List(Position P, List *L)
+{
+    if (P > L->Last || P < 1)
+    {
+        printf("vi tri khong hop le");
+    }
+    else
+    {
+        if (Empty_List(*L))
+        {
+            printf("Danh sach rong");
+        }
+        else
+        {
+            Position i;
+            for (i = P; i < L->Last; i++)
+            {
+                // mang vi tri [P - 1] se duoc gan bang P
+
+                // Array 		0	1	2	3	4	5
+
+                // Position 		1	2	3	4	5	6
+
+                // Element		2	7	5	8	6	Last
+
+                /*
+
+                Delete (4, L)
+
+                //Array			0	1	2	4	5
+
+                //Position 		1	2	3	5	6
+
+                //Element		2	7	5	6	Last
+                */
+                L->Elements[i - 1] = L->Elements[i];
+            }
+        }
+        L->Last--;
+    }
+}
+
+void Print_List(List L)
+{
+    Position P;
+
+    P = FirstList(L);
+
+    printf("bat dau danh sach");
+
+    while (P != EndList(L))
+    {
+
+        printf("%d\t", Retrieve(P, L));
+
+        P = Next(P, L);
+    }
+}
+
+void Read_List(List *L)
+{
+    int i, N;
+
+    ElementType X;
+
+    MakeNull_List(L);
+
+    printf("Nhap vao so phan tu co trong danh sach: ");
+
+    scanf("%d", &N);
+    fflush(stdin);
+
+    for (i = 1; i <= N; i++)
+    {
+
+        printf("\nPhan tu thu %d la ", i);
+
+        scanf("%d", &X);
+
+        fflush(stdin);
+
+        Insert_List(X, EndList(*L), L);
+    }
+}
+
+Position Locate(ElementType X, List L)
+{
+
+    Position P;
+
+    int found = 0;
+
+    P = FirstList(L);
+
+    while ((P != EndList(L)) && (found == 0))
+    {
+        if (Retrieve(P, L) == X)
+        {
+            found = 1;
+        }
+        else
+            P = Next(P, L);
+    }
+    return P;
+}
